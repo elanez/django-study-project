9f02bfd610130f9f237790325cf9217808c640c2
Elan Fernandez <fernandezelan@gmail.com>
2023-04-24 19:21:00 +0800

    Added server side validation logic

--- tasks/views.py
+++ tasks/views.py
@@ -5,7 +5,6 @@ tasks = ["foo", "bar", "baz"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -14,6 +13,17 @@ def index(request):
     })
 
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST) # request.POST gets the value the user submitted
+        if form.is_valid():
+            task = form.cleaned_data["task"] # access all the data user submitted
+            tasks.append(task)
+        else:
+            # return the input back to the user
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
